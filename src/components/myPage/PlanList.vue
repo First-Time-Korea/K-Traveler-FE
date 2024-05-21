37e75a4715de9c20f7f4a72ac678e46508584cef
jiwon718 <wldnjs7851@naver.com>
2024-05-22 00:13:07 +0900

    feat: 마이페이지 나의 여행 계획/여행지 네비게이션바 구현

--- src/components/myPage/PlanList.vue
+++ src/components/myPage/PlanList.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ProfileItem from "@/components/myPage/item/ProfileItem.vue";
+import PlanAndAttractionNav from "@/components/myPage/item/PlanAndAttractionNav.vue";
 import PlanListItem from "@/components/myPage/item/PlanListItem.vue";
 </script>
 
 <template>
   <div class="flex justify-center flex-col items-center">
     <ProfileItem />
+    <PlanAndAttractionNav />
     <div class="w-[60%] grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
       <PlanListItem />
       <PlanListItem />
---
+++ src/components/myPage/item/PlanAndAttractionNav.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="flex divide-x divide-third-300/70 row w-[65%]">
+    <button
+      class="w-1/2 px-6 py-3 font-sans text-xs font-bold text-center text-third-300 uppercase align-middle transition-all border-r-0 rounded-lg rounded-r-none select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-third-400/10 active:bg-third-400/20"
+      type="button"
+    >
+      PLAN
+    </button>
+    <button
+      class="w-1/2 px-6 py-3 font-sans text-xs font-bold text-center text-third-300 uppercase align-middle transition-all rounded-lg rounded-l-none select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-third-400/10 active:bg-third-400/20"
+      type="button"
+    >
+      ATTRACTION
+    </button>
+  </div>
+</template>
+
+<style></style>
