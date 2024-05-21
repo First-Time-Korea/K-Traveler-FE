b0711595f231c156d5e2a4e001bafc95dccfb3ed
jiwon718 <wldnjs7851@naver.com>
2024-05-21 10:57:26 +0900

    fix: 여행 후기 댓글 리스트 스크롤 오류 해결

--- src/components/board/item/BoardDetailItem.vue
+++ src/components/board/item/BoardDetailItem.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useMemberStore } from "@/stores/member";
 import { getArticle, deleteArticle } from "@/api/board";
@@ -87,23 +87,6 @@ const tryDeleteArticle = () => {
   );
 };
 
-const leftBody = ref(null);
-const isShowedCommentList = ref(false);
-const commentListHeight = computed(() => {
-  if (leftBody.value === null) {
-    return;
-  } else {
-    let result = (leftBody.value.clientHeight / 100) * 90;
-    isShowedCommentList.value = true;
-    return result;
-  }
-});
-const commentListStyle = computed(() => {
-  return {
-    height: `${commentListHeight.value}px`,
-  };
-});
-
 const goModify = () => {
   router.push({ name: "board-modify", params: { articleid: article.id } });
 };
@@ -112,11 +95,11 @@ const goModify = () => {
 <template>
   <div class="flex justify-center">
     <div
-      class="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl card-width min-h-[200px]"
+      class="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl card-width min-h-[200px] max-h-[1200px]"
     >
-      <div class="flex flex-row p-2">
+      <div class="flex flex-row p-2 h-full">
         <!-- 여행 후기 자세한 정보 -->
-        <div class="flex-1 m-4" ref="leftBody">
+        <div class="flex-1 m-4 h-[90%]">
           <img
             class="w-full rounded-lg object-cover object-center"
             :src="imgSrc"
@@ -138,61 +121,56 @@ const goModify = () => {
             <p class="block font-sans text-xs antialiased font-light leading-normal text-inherit">
               {{ article.time }}
             </p>
-          </div>
-
-          <!-- 수정 & 삭제 버튼 -->
-          <div class="flex justify-end mt-4" v-if="isShowedModifyButtons">
-            <button
-              class="mr-1 flex justify-center items-center relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[40px] h-8 max-h-[40px] rounded-lg text-xs text-blue-500 hover:bg-blue-700/10 active:bg-blue-700/20"
-              type="button"
-              @click="goModify"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6"
+            <!-- 수정 & 삭제 버튼 -->
+            <div class="flex justify-end mt-4" v-if="isShowedModifyButtons">
+              <button
+                class="mr-1 flex justify-center items-center relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[40px] h-8 max-h-[40px] rounded-lg text-xs text-blue-500 hover:bg-blue-700/10 active:bg-blue-700/20"
+                type="button"
+                @click="goModify"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                />
-              </svg>
-            </button>
-            <button
-              class="flex justify-center items-center relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[40px] h-8 max-h-[40px] rounded-lg text-xs text-red-500 hover:bg-red-700/10 active:bg-red-700/20"
-              type="button"
-              @click="tryDeleteArticle"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-5 h-5"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                  />
+                </svg>
+              </button>
+              <button
+                class="flex justify-center items-center relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[40px] h-8 max-h-[40px] rounded-lg text-xs text-red-500 hover:bg-red-700/10 active:bg-red-700/20"
+                type="button"
+                @click="tryDeleteArticle"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-5 h-5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
 
         <!-- 여행 후기 댓글 -->
-        <div class="flex-1 m-4 flex flex-col justify-between">
+        <div class="flex-1 m-4 flex flex-col justify-between h-full pb-8">
           <!-- 댓글 리스트 -->
-          <div
-            class="overflow-y-auto scroll"
-            :style="commentListStyle"
-            v-show="isShowedCommentList"
-          >
+          <div class="overflow-y-auto scroll max-h-3/4">
             <CommentListItem v-for="comment in comments" :key="comment.id" />
           </div>
 
