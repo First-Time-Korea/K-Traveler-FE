18476c5e6a073c76e40ba4924caebe4b29754f4d
jiwon718 <wldnjs7851@naver.com>
2024-05-21 23:59:15 +0900

    feat: 마이페이지 프로필 아이템 구현

--- src/components/myPage/PlanList.vue
+++ src/components/myPage/PlanList.vue
@@ -1,9 +1,11 @@
 <script setup>
+import ProfileItem from "@/components/myPage/item/ProfileItem.vue";
 import PlanListItem from "@/components/myPage/item/PlanListItem.vue";
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center flex-col items-center">
+    <ProfileItem />
     <div class="w-[60%] grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
       <PlanListItem />
       <PlanListItem />
---
+++ src/components/myPage/item/ProfileItem.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { useMemberStore } from "@/stores/member";
+
+const store = useMemberStore();
+
+const userId = ref();
+
+onMounted(() => {
+  if (store.userInfo !== null) {
+    userId.value = store.userInfo.id;
+  } else {
+    userId.value = "USERID";
+  }
+});
+</script>
+
+<template>
+  <div class="flex justify-center flex-col items-center">
+    <img
+      src="https://docs.material-tailwind.com/img/face-2.jpg"
+      alt="avatar"
+      class="relative inline-block h-[110px] w-[110px] !rounded-full object-cover object-center"
+    />
+    <h4
+      class="mt-6 block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-inherit"
+    >
+      {{ userId }}
+    </h4>
+    <button
+      class="mt-2 px-4 py-2 font-sans text-xs font-bold text-center text-first-300 uppercase align-middle transition-all rounded-lg select-none hover:bg-first-400/10 active:bg-first-400/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Modify My Information
+    </button>
+  </div>
+</template>
+
+<style></style>
