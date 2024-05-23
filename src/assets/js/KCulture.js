f2959d9bc6f0eef8609c4a8fa88ebe51ab3307f3
jiwon718 <wldnjs7851@naver.com>
2024-05-23 13:06:30 +0900

    temp: K-Culture 이미지 추가

--- src/assets/js/KCulture.js
+++ src/assets/js/KCulture.js
@@ -3,28 +3,28 @@ const kCultures = {
         {
             code: "E01",
             img: {
-                src: "@/assets/img/kCulture/drama/mr-sunshine.jpg",
+                src: "src/assets/img/kCulture/drama/mr-sunshine.jpg",
                 alt: "K-Drama: Mr.Sunshine"
             }
         }, 
         {
             code: "E01",
             img: {
-                src: "@/assets/img/kCulture/drama/answer-me-1988.jpg",
+                src: "src/assets/img/kCulture/drama/answer-me-1988.jpg",
                 alt: "K-Drama: Answer me 1988"
             }
         }, 
         {
             code: "E01",
             img: {
-                src: "@/assets/img/kCulture/drama/kingdom.jpg",
+                src: "src/assets/img/kCulture/drama/kingdom.jpg",
                 alt: "K-Drama: Kingdom"
             }
         }, 
         {
             code: "E01",
             img: {
-                src: "@/assets/img/kCulture/drama/twenty-five-twenty-one.jpg",
+                src: "src/assets/img/kCulture/drama/twenty-five-twenty-one.jpg",
                 alt: "K-Drama: Twenty-Five Twenty-One"
             }
         }
@@ -33,29 +33,29 @@ const kCultures = {
         {
             code: "E02",
             img: {
-                src: "@/assets/img/kCulture/movie/parasite.jpg",
+                src: "src/assets/img/kCulture/movie/parasite.jpg",
                 alt: "K-Movie: Parasite"
             }
         }, 
         {
             code: "E02",
             img: {
-                src: "",
-                alt: "K-Movie"
+                src: "src/assets/img/kCulture/movie/christmas-in-august.jpg",
+                alt: "K-Movie: Christmas in August"
             }
         }, 
         {
             code: "E02",
             img: {
-                src: "",
-                alt: "K-Movie"
+                src: "src/assets/img/kCulture/movie/brotherhood.jpg",
+                alt: "K-Movie: Brotherhood"
             }
         }, 
         {
             code: "E02",
             img: {
-                src: "",
-                alt: "K-Movie"
+                src: "src/assets/img/kCulture/movie/little-forest.jpg",
+                alt: "K-Movie: Little Forest"
             }
         }
     ],
@@ -63,29 +63,29 @@ const kCultures = {
         {
             code: "E03",
             img: {
-                src: "",
-                alt: "K-Entertainment"
+                src: "src/assets/img/kCulture/entertainment/house-on-wheels.jpg",
+                alt: "K-Entertainment: House on Wheels"
             }
         }, 
         {
             code: "E03",
             img: {
-                src: "",
-                alt: "K-Entertainment"
+                src: "src/assets/img/kCulture/entertainment/one-night-two-days.jpg",
+                alt: "K-Entertainment: One Night Two Days"
             }
         }, 
         {
             code: "E03",
             img: {
-                src: "",
-                alt: "K-Entertainment"
+                src: "src/assets/img/kCulture/entertainment/running-man.jpg",
+                alt: "K-Entertainment: Running Man"
             }
         }, 
         {
             code: "E03",
             img: {
-                src: "",
-                alt: "K-Entertainment"
+                src: "src/assets/img/kCulture/entertainment/seoul-hillbilly.jpg",
+                alt: "K-Entertainment: Seoul Hillbilly"
             }
         }
     ],
@@ -93,29 +93,29 @@ const kCultures = {
         {
             code: "E04",
             img: {
-                src: "",
-                alt: "K-Artist"
+                src: "src/assets/img/kCulture/artist/damyang-metasequoia-lined-road.jpg",
+                alt: "K-Artist: Damyang Metasequoia-lined Road(Yu Yeon-seok)"
             }
         }, 
         {
             code: "E04",
             img: {
-                src: "",
-                alt: "K-Artist"
+                src: "src/assets/img/kCulture/artist/bukhansan-mountain.jpg",
+                alt: "K-Artist: Bukhansan Mountain(Gian84)"
             }
         },
         {
             code: "E04",
             img: {
-                src: "",
-                alt: "K-Artist"
+                src: "src/assets/img/kCulture/artist/mt-hanlla.jpg",
+                alt: "K-Artist: Mt.Hanlla(Kwon Yu-ri)"
             }
         },
         {
             code: "E04",
             img: {
-                src: "",
-                alt: "K-Artist"
+                src: "src/assets/img/kCulture/artist/hyangho-beach.jpg",
+                alt: "K-Artist: Hyangho Beach(BTS)"
             }
         }
     ]
